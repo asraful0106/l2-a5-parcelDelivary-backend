932da354c20289f48b31460f709c55a28343dc3e
asraful0106 <asrafulapf@gmail.com>
2025-08-07 20:45:49 +0600

    reafctor: gloabalErrorHandeler and notFound middlewares positioned changed

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,13 @@ import { notFound } from "./app/middlewares/notFound";
 
 const app = express();
 
-// MidleWears
-app.use(globalErrorHandler); //For handeling errors
-app.use(notFound); //For handeling not found routes
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hey!!! Server is running.");
-})
+});
+
+// MidleWears
+app.use(globalErrorHandler); //For handeling errors
+app.use(notFound); //For handeling not found routes
 
 export default app;
